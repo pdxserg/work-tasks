3bf62e8f05490e8b74e530ea9d436514cc07264c
SergeyStarchenko <pdxserg@gmail.com>
2024-08-31 12:57:30 -0700

    create tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const tasks= [
+        {id:"1", title: "React", isDone:false},
+        {id:"2", title: "Hmtl", isDone:false},
+    ]
   return (
     <div className="App">
-      hello
+        <div className="task-conteiner">
+            <div className="task">
+                <input type="checkbox" checked={tasks[0].isDone}/>
+                <p>{tasks[0].title}</p>
+                <button>x</button>
+            </div>
+
+        </div>
+        <div>
+            <h2>{tasks[1].title}</h2>
+            <input type="checkbox" checked={tasks[1].isDone}/>
+        </div>
+
+
+
     </div>
   );
 }
