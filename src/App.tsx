2757edb8b04e8b0ab15065d26aacb3ef8dfb25f8
SergeyStarchenko <pdxserg@gmail.com>
2024-08-31 23:14:39 -0700

    add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
+import {v1} from "uuid";
 
 
-export type TasksPropsType ={
-    tasks: TaskPropsType[]
+export type TasksPropsType = {
+	tasks: TaskPropsType[]
 }
-export type TaskPropsType={
-    id: string
-    title: string
-    isDone:boolean
+export type TaskPropsType = {
+	id: string
+	title: string
+	isDone: boolean
 }
+
 function App() {
-    const tasks:TaskPropsType[]= [
-        {id:"1", title: "React", isDone:false},
-        {id:"2", title: "Hmtl", isDone:false},
-        {id:"3", title: "Hmtl", isDone:false},
-        {id:"4", title: "Hmtl", isDone:false},
-    ]
-  return (
-    <div className="App">
-<Todolist title={"Study"} tasks={tasks}/>
-<Todolist title={"buy"} tasks={tasks}/>
-
-    </div>
-  );
+	const tasks: TaskPropsType[] = [
+		{id: v1(), title: "React", isDone: false},
+		{id: v1(), title: "Hmtl", isDone: false},
+		{id: v1(), title: "Hmtl", isDone: false},
+		{id: v1(), title: "Hmtl", isDone: false},
+	]
+	return (
+		<div className="App">
+			<Todolist
+				title={"Study"}
+				tasks={tasks}
+			/>
+		</div>
+	);
 }
+
 export default App;
