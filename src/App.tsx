e75007fb3da3faead126e8bed48995a76ccf7aa5
SergeyStarchenko <pdxserg@gmail.com>
2024-08-31 22:02:03 -0700

    filling   task

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,18 @@ function App() {
   return (
     <div className="App">
         <div className="task-conteiner">
-            <div className="task">
-                <input type="checkbox" checked={tasks[0].isDone}/>
-                <p>{tasks[0].title}</p>
-                <button>x</button>
+            <div >
+                <h3>Study</h3>
+                <div>
+                    <input type="text"/>
+                    <button>+</button>
+                </div>
+                <div>
+                    <input type="checkbox" checked={tasks[0].isDone}/>
+                    <span>{tasks[0].title}</span>
+                    <button>x</button>
+                </div>
+
             </div>
 
         </div>
