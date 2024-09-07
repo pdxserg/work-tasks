9ea9172946266d3c72f7ded9114bef365d0b4728
SergeyStarchenko <pdxserg@gmail.com>
2024-09-07 15:37:03 -0700

    create Associativ arr after refact...

--- src/App.tsx
+++ src/App.tsx
@@ -12,36 +12,43 @@ export type TaskPropsType = {
 	title: string
 	isDone: boolean
 }
-type TodolistsPropsType={
+type TodolistType={
 	id:string
 	title:string
 	filter:FilterTodolist
 }
 
 function App() {
-	let [todolists, setTodolists] = useState<TodolistsPropsType[]>([
-		{ id: v1(), title: 'What to learn', filter: 'all' },
-		{ id: v1(), title: 'What to buy', filter: 'all' },
+	let todolistID1 = v1()
+	let todolistID2 = v1()
+
+	let [todolists, setTodolists] = useState<TodolistType[]>([
+		{ id: todolistID1, title: 'What to learn', filter: 'all' },
+		{ id: todolistID2, title: 'What to buy', filter: 'all' },
 	])
-	const [tasks, setTasks] = useState<TaskPropsType[]>(
-		[
-			{id: v1(), title: "React", isDone: false},
-			{id: v1(), title: "Hmtl", isDone: true},
-			{id: v1(), title: "Hmtl", isDone: false},
-			{id: v1(), title: "Hmtl", isDone: false},
-		]
-	)
+
+	let [tasks, setTasks] = useState({
+		[todolistID1]: [
+			{ id: v1(), title: 'HTML&CSS', isDone: true },
+			{ id: v1(), title: 'JS', isDone: true },
+			{ id: v1(), title: 'ReactJS', isDone: false },
+		],
+		[todolistID2]: [
+			{ id: v1(), title: 'Rest API', isDone: true },
+			{ id: v1(), title: 'GraphQL', isDone: false },
+		],
+	})
 const checkBoxHandler=(id: string, isDone:boolean)=>{
-	 setTasks(tasks.map(task => task.id === id?  {...task, isDone} : task))
+	 // setTasks(tasks.map(task => task.id === id?  {...task, isDone} : task))
 
 }
 
 	const removeTask =(id:string)=>{
-		setTasks(tasks.filter(t=>t.id !== id))
+		// setTasks(tasks.filter(t=>t.id !== id))
 	}
 	const createTask =(title: string)=>{
 		const newTask = {id: v1(), title: title, isDone: false}
-		setTasks([newTask, ...tasks ])
+		// setTasks([newTask, ...tasks ])
 	}
 	return (
 		<div className="App">
@@ -49,8 +56,8 @@ const checkBoxHandler=(id: string, isDone:boolean)=>{
 			{todolists.map(t=>{
 				return(
 					<Todolist
-						title={}
-						tasks={tasks}
+						title={t.title}
+						tasks={tasks[t.id]}
 						removeTask={removeTask}
 						checkBoxHandler={checkBoxHandler}
 						createTask={createTask}
