a1f0587f038cf8d777cee17eb510689aeae78cbd
SergeyStarchenko <pdxserg@gmail.com>
2024-09-15 18:00:12 -0700

    refactoring tasks-reducer.ts

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -24,9 +24,9 @@ export const tasksReducer = (state = initialstate, action: ActionsType): TasksSt
 			})
 			return stateCopy
 		}
-		case "REMOVE-TASK": {
+		case "REMOVE-TASK":
 			return {...state, [action.todolistID]: state[action.todolistID].filter(t => t.id !== action.id)}
-		}
+
 		case "UPDATE-TASK": {
 			return {
 				...state, [action.todolistID]: state[action.todolistID]
@@ -36,7 +36,6 @@ export const tasksReducer = (state = initialstate, action: ActionsType): TasksSt
 		case "CREATE-TASK": {
 			const newTask = action.newTask
 			return {...state, [action.newTask.todoListId]: [newTask, ...state[action.newTask.todoListId]]}
-
 		}
 
 		case "REMOVE-TODOLIST": {
@@ -90,26 +89,24 @@ const setTasksAC = (tasks: TaskType[], todolistId: string) => {
 
 //THUNK
 export const setTasksTC = (todolistId: string) => (dispatch: Dispatch) => {
-
 	todolistsAPI.getTasks(todolistId)
 		.then((res) => {
 			return dispatch(setTasksAC(res.data.items, todolistId))
 		})
 }
 export const createTaskTC = (todolistId: string, title: string) => (dispatch: Dispatch) => {
-
 	todolistsAPI.createTask(todolistId, title)
 		.then((res) => {
 			return dispatch(createTaskAC(res.data.data.item))
 		})
 }
 export const deleteTaskTC = (todolistId: string, taskId: string) => (dispatch: Dispatch) => {
-
 	todolistsAPI.deleteTask(todolistId, taskId)
 		.then(() => {
 			return dispatch(removeTaskAC(todolistId, taskId))
 		})
 }
+
 type UpdateDomainTaskModelType = {
 	title?: string
 	description?: string
@@ -121,7 +118,6 @@ type UpdateDomainTaskModelType = {
 export const updateTaskTC = (todolistId: string, taskId: string, domainModel: UpdateDomainTaskModelType) => (dispatch: Dispatch, getState: () => AppRootStateType) => {
 	const state = getState()
 	if (state.tasks) {
-
 		// @ts-ignore
 		const task = state.tasks[todolistId].find(t => t.id === taskId)
 
@@ -134,12 +130,9 @@ export const updateTaskTC = (todolistId: string, taskId: string, domainModel: Up
 			status: task.status,
 			...domainModel
 		}
-
 		todolistsAPI.updateTask(todolistId, taskId, apiModel)
 			.then(() => {
 				return dispatch(updateTaskAC(todolistId, taskId, domainModel))
 			})
 	}
-
-
 }
